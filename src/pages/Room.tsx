7a5f551fd9a043086fbca248c27bd5121552ed0b
Nestor-barraza <nestorbarrazaotalvarez@hotmail.com>
2023-09-25 12:03:45 -0500

    ..

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -29,8 +29,7 @@ export const Room = () => {
     }, [id, setRoomId]);
 
       
-      
-    }      
+       
     /*    const screenSharingVideo =
            screenSharingId === userId
                ? screenStream
