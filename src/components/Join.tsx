2fd39d08dc4ffd0a8db79d39b62bc15d9c1332f2
Nestor-barraza <nestorbarrazaotalvarez@hotmail.com>
2023-09-25 11:59:16 -0500

    ...

--- src/components/Join.tsx
+++ src/components/Join.tsx
@@ -1,17 +1,18 @@
-import { NameInput } from "../common/Name";
-import { Button } from "./common/Button";
+import { useEffect } from 'react';
 import { ws } from "../ws";
 
 export const Join: React.FC = () => {
     const createRoom = () => {
         ws.emit("create-room");
     };
+
+
+    useEffect(() => {
+        createRoom();
+    }, []); 
+
     return (
-        <div className=" flex flex-col">
-            <NameInput />
-            <Button onClick={createRoom} className="py-2 px-8 text-xl">
-                Start new meeting
-            </Button>
+        <div className="flex flex-col">
         </div>
     );
 };
