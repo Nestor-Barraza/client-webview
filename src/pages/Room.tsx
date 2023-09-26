ed2f2804616365099de44d0cc19f77f07d2966db
Nestor-barraza <nestorbarrazaotalvarez@hotmail.com>
2023-09-25 20:23:30 -0500

    .

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -6,7 +6,8 @@ import { UserContext } from "../context/UserContext";
 
 export const Room = () => {
     const { id } = useParams();
-    const { stream, setRoomId } = useContext(RoomContext);
+    const { stream, setRoomId } =
+        useContext(RoomContext);
     const { userName, userId } = useContext(UserContext);
 
     function newUserName(): string {
@@ -14,37 +15,21 @@ export const Room = () => {
         const digitosAleatorios = Math.floor(1000 + Math.random() * 9000); 
         
         return usuario + digitosAleatorios.toString();
-    }
+      }
+    
 
     useEffect(() => {
-        async function setupCamera() {
-            if (stream) {
-                try {
-                    const constraints = {
-                        video: {
-                            width: { ideal: 640 }, 
-                            height: { ideal: 640 }, 
-                            frameRate: { ideal: 20 } 
-                        },
-                        audio: true
-                    };
-
-                    const mediaStream = await navigator.mediaDevices.getUserMedia(constraints);
-
-                    ws.emit("join-room", { roomId: '37d164ce-8e7a-47dc-86f5-9bc5d3bfcb47', peerId: userId, userName: newUserName(), stream: mediaStream });
-                } catch (error) {
-                    console.error("Error al obtener acceso a la cámara:", error);
-                }
-            }
-        }
-
-        setupCamera();
+        if (stream)
+            ws.emit("join-room", { roomId: '37d164ce-8e7a-47dc-86f5-9bc5d3bfcb47', peerId: userId, userName: newUserName() });
     }, [id, userId, stream, userName]);
 
     useEffect(() => {
         setRoomId(id ?? "");
     }, [id, setRoomId]);
 
+      
+       
+   
     return (
         <div className="flex flex-col min-h-screen"/>
     );
