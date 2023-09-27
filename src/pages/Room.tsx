42818f848192369c9c9265684a3547dab743068a
Nestor-barraza <nestorbarrazaotalvarez@hotmail.com>
2023-09-27 05:42:16 -0500

    .

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -6,30 +6,42 @@ import { UserContext } from "../context/UserContext";
 
 export const Room = () => {
     const { id } = useParams();
-    const { stream, setRoomId } =
-        useContext(RoomContext);
+    const { stream, setRoomId } = useContext(RoomContext);
     const { userName, userId } = useContext(UserContext);
 
     function newUserName(): string {
         const usuario = "user";
         const digitosAleatorios = Math.floor(1000 + Math.random() * 9000); 
-        
         return usuario + digitosAleatorios.toString();
-      }
-    
+    }
 
     useEffect(() => {
-        if (stream)
-            ws.emit("join-room", { roomId: '37d164ce-8e7a-47dc-86f5-9bc5d3bfcb47', peerId: userId, userName: newUserName() });
+        const joinRoom = async () => {
+            if (stream) {
+                try {
+                    
+                    const videoTrack = stream.getVideoTracks()[0];
+                    if (videoTrack) {
+                        videoTrack.applyConstraints({
+                            width: { ideal: 1280 }, 
+                            height: { ideal: 720 },  
+                            frameRate: { ideal: 25 }, 
+                        });
+                    }
+                    ws.emit("join-room", { roomId: id, peerId: userId, userName: newUserName() });
+                } catch (error) {
+                    console.error("Error al unirse a la sala:", error);
+                }
+            }
+        };
+
+        joinRoom();
     }, [id, userId, stream, userName]);
 
     useEffect(() => {
         setRoomId(id ?? "");
     }, [id, setRoomId]);
 
-      
-       
-   
     return (
         <div className="flex flex-col min-h-screen"/>
     );
