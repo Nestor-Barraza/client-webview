c196bc8fdb801296c9a4e42e137bfdf312ec2ad2
Antonio Alvarez <antonioalvarez713@gmail.com>
2023-10-01 18:01:05 -0400

    test logs parseJwt

--- src/utils/parseJwt.ts
+++ src/utils/parseJwt.ts
@@ -15,6 +15,6 @@ export function parseJwt (token: string): TokenPayload {
   var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
       return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
   }).join(''));
-
+  console.log({jsonPayload})
   return JSON.parse(jsonPayload) as TokenPayload;
 }
